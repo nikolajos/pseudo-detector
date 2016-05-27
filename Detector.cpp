a0c823b5622aa26d876e6ff318ca3dd95200c55e
nikolajos <nikolajos@users.noreply.github.com>
2016-05-27 11:21:02 +0200

    added old code

---
+++ Detector.cpp
@@ -0,0 +1,176 @@
+#include "Detector.h"
+#include "TRandom.h"
+#include "math.h"
+#include<iostream>
+
+bool SubDetector::inside(int pdgid, TLorentzVector p)
+{
+    if (allowid.count(pdgid)==0) return false;
+    std::map<float, bool>::iterator eta = --alloweta.upper_bound( fabs(p.Eta()) );
+    if ( !(eta->second) ) return false;
+    std::map<float, bool>::iterator pt = --allowpt.upper_bound( p.Pt() );
+    if ( !(pt->second) ) return false;
+    return true;
+}
+
+double SubDetector::sigma(TLorentzVector p)
+{
+    if (type == DetType::Calo)
+    {
+	return sqrt(siga*siga/p.E()+sigb*sigb)*p.E();
+    }
+    else if (type == DetType::Tracker) 
+    {
+	return sqrt(siga*siga*p.Perp2()+sigb*sigb); 
+    }
+    else if (type == DetType::Muon)
+    {
+	return siga*p.Pt();
+    }
+    else
+    {
+	return 1000000;
+    }
+}
+
+TLorentzVector SubDetector::Smear(TLorentzVector p)
+{
+    TRandom ran(123456789);
+    if (type == DetType::Calo)
+    {
+	double newE = p.E()+ran.Gaus(0,sigma(p));
+	double c = sqrt(newE*newE - p.M2() ) / p.P();
+	p = TLorentzVector(c*p.Vect(), newE);
+	    
+    }
+    else if (type == DetType::Tracker || type == DetType::Muon)
+    {
+	double newPt = p.Pt()+ran.Gaus(0,sigma(p));
+	double newEta = acosh(sqrt(1+p.Pz()*p.Pz()/(newPt*newPt)));
+	p.SetPtEtaPhiM(newPt, newEta, p.Phi(), p.M());
+    }
+    return p;
+}
+
+bool Detector::InsideAcceptance(int pdgid, TLorentzVector p)
+{
+    for (std::vector<base_detector*>::iterator it = subsystems.begin(); it != subsystems.end(); ++it)
+    {
+	if ((*it)->inside(pdgid,p))
+	{
+	    //std::cout << "Detected by " << (*it)->name << std::endl;
+	    return true;
+	}
+    }
+    return false;
+}
+
+// TODO eta/phi smearing
+TLorentzVector Detector::Smear(int pdgid, TLorentzVector p)
+{
+    std::vector<base_detector*>::iterator min = subsystems.begin();
+    double minsig = 1000000.0;
+    double sig;
+    for (std::vector<base_detector*>::iterator it = subsystems.begin(); it != subsystems.end(); ++it)
+    {
+	sig = (*it)->sigma(p);
+	if ( (*it)->inside(pdgid,p) && sig < minsig )
+	{
+	    minsig = sig;
+	    min = it;
+	}
+    }
+    //std::cout << "Smeared by " << (*min)->name << std::endl;
+    //std::cout << "with sigma " << (*min)->sigma(p) << std::endl;
+    return (*min)->Smear(p);
+}
+
+void Detector::ATLAS()
+{
+    
+    SubDetector* sub = new SubDetector("Tracker", DetType::Tracker, 0.0005, 0.01);
+    // Tracker accepts electrons
+    sub->allowid.insert(11);
+    sub->allowid.insert(-11);
+    // Muons
+    sub->allowid.insert(13);
+    sub->allowid.insert(-13);
+    // Protons
+    sub->allowid.insert(2212);
+    sub->allowid.insert(-2212);
+    // Pions
+    sub->allowid.insert(211);
+    sub->allowid.insert(-211);
+    // Charged Kaons
+    sub->allowid.insert(321);
+    sub->allowid.insert(-321);
+
+    // Tracker covers |eta| < 2.5
+    sub->alloweta[0] = true;
+    sub->alloweta[2.5] = false;
+    
+    sub->allowpt[0] = false;
+    sub->allowpt[0.1] = true;
+    subsystems.push_back(sub);
+
+    sub = new SubDetector("ECAL",DetType::Calo,0.1,0.007);
+    // ECAL sees photons and electrons
+    sub->allowid.insert(22);
+    sub->allowid.insert(11);
+    sub->allowid.insert(-11);
+    // ECAL covers |eta| < 3.2
+    sub->alloweta[0] = true; // Barrel
+    sub->alloweta[1.37] = false;
+    sub->alloweta[1.52] = true; // Endcap outer wheel
+    sub->alloweta[2.47] = false;
+    sub->alloweta[2.5] = true; // Endcap inner wheel
+    sub->alloweta[3.2] = false;
+    
+    sub->allowpt[0] = false;
+    sub->allowpt[25] = true;
+
+    subsystems.push_back(sub);
+    
+
+    sub = new SubDetector("HCAL",DetType::Calo,0.75,0.05);
+    // HCAL sees protons,
+    sub->allowid.insert(2212);
+    sub->allowid.insert(-2212);
+    // Neutrons
+    sub->allowid.insert(2112);
+    sub->allowid.insert(-2112);
+    // Pions
+    sub->allowid.insert(211);
+    sub->allowid.insert(-211);
+    // Charged Kaons
+    sub->allowid.insert(321);
+    sub->allowid.insert(-321);
+    // neutral K_L
+    sub->allowid.insert(130);
+    
+    // HCAL covers |eta| < 4.9
+    sub->alloweta[0] = true; // Barrel
+    sub->alloweta[1.47] = false;
+    sub->alloweta[1.52] = true; // Endcap
+    sub->alloweta[3.07] = false;
+    sub->alloweta[3.22] = true; // FCal
+    sub->alloweta[4.9] = false;
+    
+    sub->allowpt[0] = false;
+    sub->allowpt[25] = true;
+    
+    subsystems.push_back(sub);
+
+    sub = new SubDetector("Muon spectrometer",DetType::Muon,0.1);
+    sub->allowid.insert(13);
+    sub->allowid.insert(-13);
+
+    sub->alloweta[0] = true;
+    sub->alloweta[2.7] = false;
+
+    sub->allowpt[0] = false;
+    sub->allowpt[3] = true;
+    
+    subsystems.push_back(sub);
+}
+
---
+++ Detector.h
@@ -0,0 +1,70 @@
+#ifndef DETECTOR_H
+#define DETECTOR_H
+
+#include "TLorentzVector.h"
+#include<vector>
+#include<map>
+#include<set>
+#include<string>
+#include<math.h>
+
+enum class Geometry {ATLAS, Custom};
+enum class DetType {Tracker, Calo, Muon, NONE};
+
+class base_detector
+{
+ public:
+    base_detector(std::string s="DetName") { name = s; }
+    virtual ~base_detector() {};
+    std::string name;
+    virtual bool inside(int, TLorentzVector) =0;
+    virtual TLorentzVector Smear(TLorentzVector p) { return p; }
+    virtual double sigma(TLorentzVector p) =0;
+};
+
+class SubDetector: public base_detector
+{
+ public:
+ SubDetector(std::string s="DetName", DetType t = DetType::NONE, double a = 1, double b = 0.1): base_detector(s), type(t), siga(a), sigb(b)
+    {
+	alloweta[0] = true;
+	allowpt[0] = false;
+    }
+    DetType type;
+    double siga; // Stochastic term in uncertainty
+    double sigb; // Constant term in uncertainty
+    std::set<int> allowid;
+    std::map<float, bool> alloweta;
+    std::map<float, bool> allowpt;
+    bool inside(int, TLorentzVector);
+    double sigma(TLorentzVector);
+    TLorentzVector Smear(TLorentzVector);
+};
+
+
+class Detector
+{
+ public:
+    Detector(Geometry geom = Geometry::ATLAS)
+    {
+	if (geom == Geometry::ATLAS) ATLAS();
+	else if (geom == Geometry::Custom) { }
+    }
+    ~Detector()
+    {
+	for (std::vector<base_detector*>::iterator it = subsystems.begin(); it != subsystems.end(); ++it)
+	{
+	    delete *it;
+	}
+    }
+    bool InsideAcceptance(int, TLorentzVector);
+    double Efficiency(int pdgid, TLorentzVector p) { return InsideAcceptance(pdgid, p); }
+    TLorentzVector Smear(int, TLorentzVector);
+
+    std::vector<base_detector*> subsystems;
+ private:
+    void ATLAS();
+};
+
+
+#endif
